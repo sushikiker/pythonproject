203cded63eec8d116eddb6ec4d8339507ed0fbf0
Гирский Андрей <andreygirskiy291@gmail.com>
2025-02-28 19:07:54 +0500

    initial commit

---
+++ infrastructure/config.py
@@ -0,0 +1,19 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: str
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    @property
+    def DATABASE_URL_asyncpg(self):
+        # postgresql+asyncpg://postgres:postgres@localhost:5432/sa
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+    
+    model_config = SettingsConfigDict(env_file="/home/easooh/Desktop/s_alch/.env")
+
+
+settings_db = Settings()
---
+++ infrastructure/database.py
@@ -0,0 +1,18 @@
+import asyncio
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase
+
+from infrastructure.config import settings_db
+
+async_engine = create_async_engine(
+    url= settings_db.DATABASE_URL_asyncpg,
+    pool_size=5,
+    max_overflow=10
+)
+
+session_fabric = async_sessionmaker(async_engine)
+
+
+
+
