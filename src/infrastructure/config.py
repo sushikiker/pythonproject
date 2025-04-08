3f2bded54fbf350ac9f372801268e9908b9874fe
Гирский Андрей <andreygirskiy291@gmail.com>
2025-04-08 21:32:54 +0500

    fix config

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -8,11 +8,19 @@ class Settings(BaseSettings):
     DB_PASS: str
     DB_NAME: str
 
+    REDIS_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
     @property
     def DATABASE_URL_asyncpg(self):
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
     
-    model_config = SettingsConfigDict(env_file="/home/easooh/Desktop/s_alch/.env")
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
 
 
 settings_db = Settings()
